4946d0e568ec66d1325ba885d4b749360f3ac168
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2023-05-17 20:06:18 -0300

    Borda div projetos e efeito luz de fundo

--- landingpage/src/components/Projetos.vue
+++ landingpage/src/components/Projetos.vue
@@ -10,52 +10,64 @@
   <div class="text-center" id="projetos">
     <h2>Meus Projetos</h2>
     <div class="container row row-cols-2 m-auto">
-        <div class="col-md-5 col-11 m-auto projeto">
-            <h3>I Want</h3>
-            <img src="../assets/projetos/IWant.png" alt="API de produtos" class="img-fluid">
-            <span>Tecnologias Usadas</span>
-            <div class="d-flex justify-content-center">
-                <img src="../assets/logos/cSharp.png" alt="logo CSharp" class="icon-img">
+        <div class="board col-md-5 m-5 brightness">
+            <div class="col-md-10 col-11 m-auto projeto">
+                <h3>I Want</h3>
+                <img src="../assets/projetos/IWant.png" alt="API de produtos" class="img-fluid">
+                <span>Tecnologias Usadas</span>
+                <div class="d-flex justify-content-center">
+                    <img src="../assets/logos/cSharp.png" alt="logo CSharp" class="icon-img">
+                </div>
             </div>
         </div>
-        <div class="col-md-5 col-11 m-auto projeto">
-            <h3>Filmaria</h3>
-            <img src="../assets/projetos/filmaria.png" alt="Site de lançamento de filmes" class="img-fluid">
-            <span>Tecnologias Usadas</span>
-            <div class="d-flex justify-content-center">
-                <img src="../assets/logos/vue.png" alt="logo vue.js" class="icon-img">
+        <div class="board col-md-5 m-5 brightness">
+            <div class="col-md-10 col-11 m-auto projeto">
+                <h3>Filmaria</h3>
+                <img src="../assets/projetos/filmaria.png" alt="Site de lançamento de filmes" class="img-fluid">
+                <span>Tecnologias Usadas</span>
+                <div class="d-flex justify-content-center">
+                    <img src="../assets/logos/vue.png" alt="logo vue.js" class="icon-img">
+                </div>
             </div>
         </div>
-        <div class="col-md-5 col-11 m-auto projeto">
-            <h3>Me Acha</h3>
-            <img src="../assets/projetos/MeAcha.png" alt="Site busca de CEPs" class="img-fluid">
-            <span>Tecnologias Usadas</span>
-            <div class="d-flex justify-content-center">
-                <img src="../assets/logos/front.png" alt="logo html css e javascript" class="icon-img">
+        <div class="board col-md-5 m-5 brightness">
+            <div class="col-md-10 col-11 m-auto projeto">
+                <h3>Me Acha</h3>
+                <img src="../assets/projetos/MeAcha.png" alt="Site busca de CEPs" class="img-fluid">
+                <span>Tecnologias Usadas</span>
+                <div class="d-flex justify-content-center">
+                    <img src="../assets/logos/front.png" alt="logo html css e javascript" class="icon-img">
+                </div>
             </div>
         </div>
-        <div class="col-md-5 col-11 m-auto projeto">
-            <h3>My Clouth Stock</h3>
-            <img src="../assets/projetos/myClouthStock.png" alt="Site para controle de estoque de roupas" class="img-fluid">
-            <span>Tecnologias Usadas</span>
-            <div class="d-flex justify-content-center">
-                <img src="../assets/logos/python.png" alt="logo python" class="icon-img">
+        <div class="board col-md-5 m-5 brightness">
+            <div class="col-md-10 col-11 m-auto projeto">
+                <h3>My Clouth Stock</h3>
+                <img src="../assets/projetos/myClouthStock.png" alt="Site para controle de estoque de roupas" class="img-fluid">
+                <span>Tecnologias Usadas</span>
+                <div class="d-flex justify-content-center">
+                    <img src="../assets/logos/python.png" alt="logo python" class="icon-img">
+                </div>
             </div>
         </div>
-        <div class="col-md-5 col-11 m-auto projeto">
-            <h3>Controle de Pedidos</h3>
-            <img src="../assets/projetos/controleDePedidos.png" alt="Site para controle de estoque de items" class="img-fluid">
-            <span>Tecnologias Usadas</span>
-            <div class="d-flex justify-content-center">
-                <img src="../assets/logos/front.png" alt="logo html css e javascript" class="icon-img">
+        <div class="board col-md-5 m-5 brightness">
+            <div class="col-md-10 col-11 m-auto projeto">
+                <h3>Controle de Pedidos</h3>
+                <img src="../assets/projetos/controleDePedidos.png" alt="Site para controle de estoque de items" class="img-fluid">
+                <span>Tecnologias Usadas</span>
+                <div class="d-flex justify-content-center">
+                    <img src="../assets/logos/front.png" alt="logo html css e javascript" class="icon-img">
+                </div>
             </div>
         </div>
-        <div class="col-md-5 col-11 m-auto projeto">
-            <h3>Downloader de Video do Youtube</h3>
-            <img src="../assets/projetos/youtubeDownloader.png" alt="Aplicativo desktop para baixar videos e audios do youtube" class="img-fluid">
-            <span>Tecnologias Usadas</span>
-            <div class="d-flex justify-content-center">
-                <img src="../assets/logos/python.png" alt="logo python" class="icon-img">
+        <div class="board col-md-5 m-5 brightness">
+            <div class="col-md-10 col-11 m-auto projeto">
+                <h3>Downloader de Video do Youtube</h3>
+                <img src="../assets/projetos/youtubeDownloader.png" alt="Aplicativo desktop para baixar videos e audios do youtube" class="img-fluid">
+                <span>Tecnologias Usadas</span>
+                <div class="d-flex justify-content-center">
+                    <img src="../assets/logos/python.png" alt="logo python" class="icon-img">
+                </div>
             </div>
         </div>
     </div>
@@ -134,5 +146,17 @@ export default {
     h3{
         font-size: 24px;
     }
+    .board{
+        border: 4px solid rgba(255, 255, 255, 0.2);
+        border-radius: 10px;
+        height: 450px;
+    }
 
+    img{
+        max-height: 340px;
+    }
+
+    .brightness{
+    box-shadow: 0px 0px 25px 10px rgba(255, 255, 255, 0.1);
+    }
 </style>
